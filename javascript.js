3596f1dc4e5706e9471e8c11accd8812a789e730
Arsén <rhos.andrea@gmail.com>
2025-08-14 19:08:33 +0200

    Add real-time password match validation with dynamic feedback

---
+++ javascript.js
@@ -0,0 +1,26 @@
+const messageElement = document.getElementById('passwordMatchMessage');
+
+function passwordMatch() {
+    const password = document.getElementById("password");
+    const passwordConfirm = document.getElementById('confirmPassword');
+
+    if (password.value === '' || passwordConfirm.value === '') {
+        messageElement.textContent = '';
+        messageElement.style.color = '';
+        return;}
+
+    if (password.value === passwordConfirm.value) {
+        messageElement.textContent = 'Passwords match!';
+        messageElement.style.color = 'green';
+     } else if (password.value === '' && passwordConfirm.value !== password.value ) {
+        messageElement.textContent = '';
+        messageElement.style.color = '';
+    }  
+     else {
+        messageElement.textContent = 'Passwords do not match!';
+        messageElement.style.color = 'red';
+    }
+}
+
+password.addEventListener('input', passwordMatch);
+    confirmPassword.addEventListener('input', passwordMatch);
